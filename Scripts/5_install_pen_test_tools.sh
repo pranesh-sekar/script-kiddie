1b1146a96a500f6a3772a0c31e6cded85efc403a
Pranesh Sekar <pranesh2014@gmail.com>
2023-08-23 01:50:06 -0400

    feat: added SecLists

--- Scripts/5_install_pen_test_tools.sh
+++ Scripts/5_install_pen_test_tools.sh
@@ -2,3 +2,9 @@
 
 # Install Cyber Security Tools
 sudo nala install nmap gobuster openvpn
+
+# Downloading SecLists
+wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O SecList.zip \
+  && unzip SecList.zip \
+  && rm -f SecList.zip \
+  && mv SecLists-master/ SecLists/
